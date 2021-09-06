dd1b08e53cf27cb2a9d05d7e9a16462306cf38a4
WaseemAnjum <66861984+fabwaseem@users.noreply.github.com>
2021-09-06 18:06:22 +0500

    Add files via upload

--- age.js
+++ age.js
@@ -1,1 +1,50 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('m a=["\\k\\o\\c\\I\\b\\s\\e","\\p\\b\\e\\1g\\j\\b\\x\\b\\c\\e\\1C\\r\\1r\\l","\\N\\f\\c\\M\\q\\e","\\1p\\q\\b\\s\\r\\1q\\b\\j\\b\\k\\e\\o\\s","\\N\\o\\q\\e\\M\\q\\e","\\N\\A\\d\\k\\L\\n\\A\\e\\c","\\1a\\A\\l\\d\\r","\\1a\\e\\l\\d\\r","\\d\\p\\b\\n\\f\\c\\n\\g\\b\\k\\o\\c\\l\\g","\\d\\p\\b\\n\\f\\c\\n\\x\\f\\c\\q\\e\\b\\g","\\d\\p\\b\\n\\f\\c\\n\\B\\o\\q\\s\\g","\\d\\p\\b\\n\\f\\c\\n\\l\\d\\r\\g","\\f\\c\\d\\x","\\f\\c\\d\\x\\1B","\\k\\j\\f\\k\\L","\\B\\f\\l\\b","\\e\\o\\p\\p\\j\\b","\\k\\j\\d\\g\\g\\K\\f\\g\\e","\\g\\B\\o\\1A","\\d\\l\\l\\1g\\I\\b\\c\\e\\K\\f\\g\\e\\b\\c\\b\\s","\\k\\B\\d\\c\\p\\b","\\l\\f\\g\\M\\j\\d\\r","\\g\\e\\r\\j\\b","\\c\\o\\c\\b","\\I\\d\\j\\q\\b\\1y\\g\\1w\\d\\e\\b","\\I\\d\\j\\q\\b","\\g\\j\\f\\k\\b","\\A\\j\\o\\k\\L","\\f\\c\\c\\b\\s\\1t\\1u\\1v\\K"];i 1l=h[a[1]](a[0]);i T=h[a[3]](a[2]);i P=h[a[3]](a[4]);i 1i=h[a[3]](a[5]);i 1x=h[a[3]](a[6]);i 1s=h[a[3]](a[7]);i Y=h[a[1]](a[8]);i 1o=h[a[1]](a[9]);i 1d=h[a[1]](a[10]);i Z=h[a[1]](a[11]);i y=h[a[1]](a[12]);i C=h[a[1]](a[13]);1i[a[19]](a[14],()=>{T[a[17]][a[16]](a[15]);P[a[17]][a[16]](a[18])});U[a[19]](a[1j],()=>{y[a[t]][a[u]]=a[1k]});F[a[19]](a[1j],()=>{C[a[t]][a[u]]=a[1k]});F[a[1b]]=w v();1l[a[19]](a[14],()=>{m O=U[a[G]];m W=F[a[G]];m S=O[a[1n]](0,4);m R=W[a[1n]](0,4);z(1m(w v(O))){y[a[t]][a[u]]=a[E]}D{z(1m(w v(W))){C[a[t]][a[u]]=a[E]}D{z(S<1h||S>1f){y[a[t]][a[u]]=a[E]}D{z(R<1h||R>1f){C[a[t]][a[u]]=a[E]}D{m V=(w v(F[a[G]])-w v(U[a[G]]))/1z;m Q=V/1c;m X=Q/1c;m 1e=X/1b;Y[a[J]]=H(V);1o[a[J]]=H(Q);1d[a[J]]=H(X);Z[a[J]]=H(1e);T[a[17]][a[16]](a[15]);P[a[17]][a[16]](a[18])}}}}})',62,101,'||||||||||_0xc173|x65|x6E|x61|x74|x69|x73|document|const|x6C|x63|x64|var|x2D|x6F|x67|x75|x79|x72|22|21|Date|new|x6D|inam|if|x62|x68|inam2|else|27|tday|25|parseInt|x76|28|x4C|x6B|x70|x2E|_0xad3bxd|output|_0xad3bx12|_0xad3bx10|_0xad3bxf|input|bday|_0xad3bx11|_0xad3bxe|_0xad3bx13|ageInSeconds|ageInDays|||||||||||x23|24|60|ageInHours|_0xad3bx14|2021|x45|1900|backBtn|20|23|convert|isNaN|26|ageInMinutes|x71|x53|x49|tDay|x48|x54|x4D|x44|bDay|x41|1000|x77|x32|x42'.split('|'),0,{}))
+const bDay = document.querySelector("#bday");
+const tDay = document.querySelector("#tday");
+const ageInSeconds = document.getElementById("age-in-seconds");
+const ageInMinutes = document.getElementById("age-in-minutes");
+const ageInHours = document.getElementById("age-in-hours");
+const ageInDays = document.getElementById("age-in-days");
+const inam2 = document.getElementById("inam2");
+
+backBtn.addEventListener("click", () => {
+  input.classList.toggle("hide");
+  output.classList.toggle("show");
+});
+bday.addEventListener("change", () => {
+    inam.style.display = "none";
+})
+tday.addEventListener("change", () => {
+    inam2.style.display = "none";
+})
+tday.valueAsDate = new Date();
+
+convert.addEventListener("click", () => {
+  var foo = bday.value;
+  var foo2 = tday.value;
+  var year = foo.slice(0, 4);
+  var year2 = foo2.slice(0, 4);
+
+if(isNaN(new Date(foo))) {
+    inam.style.display = "block";
+    
+}else if(isNaN(new Date(foo2))){
+    inam2.style.display = "block";
+}else if(year < 1900 || year > 2021){
+        inam.style.display = "block";
+}
+else if(year2 < 1900 || year2 > 2021){
+        inam2.style.display = "block";
+}else{
+    var seconds = (new Date(tday.value) - new Date(bday.value)) / 1000;
+    var minutes = seconds / 60;
+    var hours = minutes / 60;
+    var days = hours / 24;
+    ageInSeconds.innerHTML = parseInt(seconds);
+    ageInMinutes.innerHTML = parseInt(minutes);
+    ageInHours.innerHTML = parseInt(hours);
+    ageInDays.innerHTML = parseInt(days);
+    input.classList.toggle("hide");
+  output.classList.toggle("show");
+      }
+
+});
